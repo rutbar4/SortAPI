c89eb2151f064955acd1902ef87212ad176cfbce
RUTA <r.bartkute@ktu.edu>
2023-09-23 17:06:48 +0300

    Timers added

--- HomeWorkTask/Utils/SortUtils/SortHandler.cs
+++ HomeWorkTask/Utils/SortUtils/SortHandler.cs
@@ -1,16 +1,57 @@
-﻿namespace HomeWorkTask.Utils.SortUtils
+﻿using System.Diagnostics;
+
+namespace HomeWorkTask.Utils.SortUtils
 {
     public static class SortHandler
     {
         public static int[] sortNumbers(int[] numbers)
         {
-            var resultBubbleSort = SortAlgorithms.BubbleSort(numbers);
-            var resultInsertionSort = SortAlgorithms.InsertionSort(numbers);
-
-            var numbersList = numbers.ToList();
-            var resultInsertionSortNew = SortAlgorithms.InsertionSortNew(numbersList);
+            var resultBubbleSort = BubbleSortAndTimer(numbers);
+            var resultInsetionSort = InsetionSortAndTimer(numbers);
+            var resultInsrtionSortNew = InsrtionSortNewAndTimer(numbers);
 
             return resultBubbleSort;
+        }
+
+        private static int[] BubbleSortAndTimer(int[] numbers)
+        {
+            var watchBubble = System.Diagnostics.Stopwatch.StartNew();
+
+            var result = SortAlgorithms.BubbleSort(numbers);
+            watchBubble.Stop();
+            var elapsedMsBubble = watchBubble.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"Bubble time: {@elapsedMsBubble}");
+            return result;
+        }
+
+        private static int[] InsrtionSortNewAndTimer(int[] numbers)
+        {
+            var watchInsertionSortNew = System.Diagnostics.Stopwatch.StartNew();
+
+            var numbersList = numbers.ToList();
+            var resultList = SortAlgorithms.InsertionSortNew(numbersList);
+
+            watchInsertionSortNew.Stop();
+            var elapsedMsInsertionSortNew = watchInsertionSortNew.Elapsed.TotalMilliseconds;
+            Console.WriteLine($"InsertionSortNew time: {@elapsedMsInsertionSortNew}");
+
+            var result = resultList.ToArray();
+            return result;
+        }
+
+        private static int[] InsetionSortAndTimer(int[] numbers)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var result = SortAlgorithms.InsertionSort(numbers);
+
+            watch.Stop();
+            var elapsedMsInsertionSort = watch.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine($"InsertionSort time: {@elapsedMsInsertionSort}");
+
+            return result;
         }
     }
 }
