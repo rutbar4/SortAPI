6df8982d07c59daaa93f9b9c79a323ed6d4733bb
RUTA <r.bartkute@ktu.edu>
2023-09-26 02:28:32 +0300

    Added unit tests: Validation, SortAlgorithms classes

---
+++ UnitTests/UnitTests/SortAlgorithmsUnitTests.cs
@@ -0,0 +1,46 @@
+using HomeWorkTask.Utils.SortUtils;
+
+namespace UnitTests.ControllersUnitTests
+{
+    public class Tests
+    {
+        private readonly int[] _inputNumbers = { 3, 1, 2, 40, 25, -3, -100 };
+
+        [Test]
+        public void BubbleSort_NumbersArray_IsCorrectlyOrdered()
+        {
+            // Act
+            var outputNumbers = SortAlgorithms.BubbleSort(_inputNumbers);  
+
+            // Assert
+            Assert.That(outputNumbers, Has.Length.EqualTo(_inputNumbers.Length));
+            Assert.That(outputNumbers, Is.Ordered);
+        }
+
+        [Test]
+        public void InsertionSort_NumbersArray_IsCorrectlyOrdered()
+        {
+            // Act
+            var outputNumbers = SortAlgorithms.InsertionSort(_inputNumbers);
+
+            // Assert
+            Assert.That(outputNumbers, Has.Length.EqualTo(_inputNumbers.Length));
+            Assert.That(outputNumbers, Is.Ordered);
+
+        }
+
+        [Test]
+        public void InsertionSortNew_NumbersList_IsCorrectlyOrdered()
+        {
+            // Arrange 
+            var inputNumbersList = _inputNumbers.ToList();
+
+            // Act
+            var outputNumbers = SortAlgorithms.InsertionSort(inputNumbersList);
+
+            // Assert
+            Assert.That(outputNumbers, Has.Count.EqualTo(inputNumbersList.Count));
+            Assert.That(outputNumbers, Is.Ordered);
+        }
+    }
+}
---
+++ UnitTests/UnitTests/ValidationUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using HomeWorkTask.Utils;
+
+namespace Tests.UnitTests
+{
+    internal class ValidationUnitTests
+    {
+        [Test]
+        public void IsRangeValid_ValidRange_true()
+        {
+            // Arrange 
+            int[] validInput = { 3, 2, 9, 4, 1, 10 };
+
+            // Act
+            var result = Validation.IsRangeValid(validInput);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsRangeValid_MoreThan10_false()
+        {
+            // Arrange 
+            int[] invalidInput = { 3, 2, 11, 4, 1 };
+
+            // Act
+            var result = Validation.IsRangeValid(invalidInput);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+        
+        [Test]
+        public void IsRangeValid_LessThan1_false()
+        {
+            // Arrange 
+            int[] invalidInput = { 3, 2, 0, 1 };
+
+            // Act
+            var result = Validation.IsRangeValid(invalidInput);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsRangeValid_LessThan1AndMoreThan10_false()
+        {
+            // Arrange 
+            int[] invalidInput = { 15, 2, 0, 1 };
+
+            // Act
+            var result = Validation.IsRangeValid(invalidInput);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
