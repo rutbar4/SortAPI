f23f171c0dc47a102b2acff2d6cf6c27af418077
RUTA <r.bartkute@ktu.edu>
2023-09-26 04:55:39 +0300

    small fix

--- HomeWorkTask/Utils/Validation.cs
+++ HomeWorkTask/Utils/Validation.cs
@@ -2,10 +2,8 @@
 {
     public static class Validation
     {
-        public static bool IsRangeValid(int[] numbers) //rename+length
+        public static bool IsRangeValid(int[] numbers)
         {
-            if (numbers.Length == 0) { return false; }
-
             foreach (int number in numbers)
             {
                 if (number < 1 || number > 10) { return false; }
